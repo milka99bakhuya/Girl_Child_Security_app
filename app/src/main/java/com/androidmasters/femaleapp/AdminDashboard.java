364280b8ce82a4d377123bf6989872e01bda4997
Milka@Wanga <milkawanga08@gmail.com>
2022-08-23 18:17:06 +0300

    user dashboard

--- app/src/main/java/com/androidmasters/femaleapp/AdminDashboard.java
+++ app/src/main/java/com/androidmasters/femaleapp/AdminDashboard.java
@@ -1,46 +1,81 @@
 package com.androidmasters.femaleapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Telephony;
+import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class AdminDashboard extends AppCompatActivity {
+public class AdminDashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     DrawerLayout drawerLayout;
     Toolbar toolbar;
     ActionBarDrawerToggle actionBarDrawerToggle;
     NavigationView navigationView;
     private FirebaseAuth firebaseAuth;
     private FirebaseUser firebaseUser;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dashboard);
-        drawerLayout=findViewById(R.id.drawer_dashboard);
-        toolbar=findViewById(R.id.toolbar);
-        navigationView=findViewById(R.id.navigation_admin);
-        actionBarDrawerToggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout = findViewById(R.id.drawer_dashboard);
+        toolbar = findViewById(R.id.toolbar);
+        navigationView = findViewById(R.id.navigation_admin);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
         actionBarDrawerToggle.syncState();
 
 
-
-        firebaseAuth= FirebaseAuth.getInstance();
-        firebaseUser=firebaseAuth.getCurrentUser();
-        if(firebaseUser==null){
-            startActivity(new Intent(getApplicationContext(),Verify_email.class));
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        if (firebaseUser == null) {
+            startActivity(new Intent(getApplicationContext(), Verify_email.class));
             finish();
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(R.id.container_admin, new MainFragment());
+            fragmentTransaction.commit();
+
 
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        drawerLayout.closeDrawer(GravityCompat.START);
+        if(item.getItemId()==R.id.home)
+        {
+            fragmentManager=getSupportFragmentManager();
+            fragmentTransaction=fragmentManager.beginTransaction();
+            fragmentTransaction.add(R.id.container_admin,new MainFragment());
+            fragmentTransaction.commit();
+        }
+        if(item.getItemId()==R.id.add_user)
+        {
+            fragmentManager=getSupportFragmentManager();
+            fragmentTransaction=fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_admin,new SecondFragment() );
+            fragmentTransaction.commit();
+        }
+
+
+        return true;
     }
 }
